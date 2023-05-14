a63487a876da0817fbf9c49c271681b8f8056681
Edgar Narvaez <enarvaez_19@alu.uabcs.mx>
2023-05-14 07:14:03 -0700

    Initial Commit

---
+++ multiplicacion/multiplicacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiplicacionApp.swift
+//  multiplicacion
+//
+//  Created by Edgar Narvaez on 14/05/23.
+//
+
+import SwiftUI
+
+@main
+struct multiplicacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
